c676411b5f19443f04a654807dc498e020c82861
tjaden <195966262+josuepalma23@users.noreply.github.com>
2026-06-10 18:54:45 -0500

    Creacion del archivo settings con variables globales y fisicas basicas

---
+++ settings.py
@@ -0,0 +1,14 @@
+## VARIABLES GLOBALES (TEST)
+
+class Ventana:
+    ANCHO = 900
+    ALTO = 700
+    TITULO = "Rally Racing"
+    COLOR_FONDO = (30, 30, 30)
+    PANTALLA = None
+    FPS = 60
+
+class Fisicas:
+    VELOCIDAD_CAMINO = 5
+    VELOCIDAD_AUTO = 8
+
