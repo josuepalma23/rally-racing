8e3a6ca1422d2f229d3995a361fe04597f868014
tjaden <195966262+josuepalma23@users.noreply.github.com>
2026-06-10 19:26:04 -0500

    main no se si este bien este paso pilas

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import settings 
 import pygame
+import sys
 
 pygame.init() 
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+pygame.quit()
+sys.exit()
+
